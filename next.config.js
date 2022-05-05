5061205f9d4bfeaf479aa32d16df6f47bbd0f7be
Ezequiel Rivas <ezequiel@innovit.cl>
2022-05-05 17:57:06 -0300

    asd

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
+  },
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
